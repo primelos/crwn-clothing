852893f9c5502c06366bfe4f79796a31ad2b878b
Ale <alearanibar@gmail.com>
2020-12-18 14:20:20 -0800

    setup structure of components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './pages/homePage'
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HomePage />
     </div>
   );
 }
 
 export default App;
+
+
+// installed
+// npm i
+// npm i node-sass
+
+// had a problem with SASS found a fix
+// npm uninstall node-sass
+// npm install node-sass@4.14.1
+
+// to remove through terminal
+// rm -rf yarn.lock
