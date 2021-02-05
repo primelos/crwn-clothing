9fa86a9100c7cc47e35a89a8ab6e35c57b72827b
Ale <alearanibar@gmail.com>
2021-02-04 23:58:26 -0800

    removed thunk and implemneted sagas with redux

--- src/pages/shop/index.jsx
+++ src/pages/shop/index.jsx
@@ -3,7 +3,7 @@ import { Route } from "react-router-dom";
 import { connect } from "react-redux";
 import CollectionsOverviewContainer from "../../components/collections-overview/collections.overview.container";
 import CollectionPageContainer from "../collection/collection.container";
-import { fetchCollectionsStartAsync } from "../../redux/shop/shop.actions";
+import { fetchCollectionsStart } from "../../redux/shop/shop.actions";
 // import {
 //   firestore,
 //   convertCollectionsSnapshotToMap,
@@ -30,8 +30,8 @@ class ShopPage extends Component {
   // unsubscribeFromSnapshot = null;
 
   componentDidMount() {
-    const { fetchCollectionsStartAsync } = this.props
-    fetchCollectionsStartAsync()
+    const { fetchCollectionsStart } = this.props
+    fetchCollectionsStart()
     // const { updateCollections } = this.props;
     // const collectionRef = firestore.collection("collections");
 
@@ -81,7 +81,7 @@ class ShopPage extends Component {
 
 
 const mapDispatchToProps = (dispatch) => ({
-  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+  fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
 });
 
 export default connect(null, mapDispatchToProps)(ShopPage);
